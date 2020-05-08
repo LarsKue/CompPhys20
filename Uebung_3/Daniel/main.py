b79a6a63513dbd0543fa4cc4064e3c965486391f
danikreuz <dani.winzeln@gmail.com>
2020-05-08 15:36:10 +0200

    next try

--- Uebung_3/Daniel/main.py
+++ Uebung_3/Daniel/main.py
@@ -43,10 +43,10 @@ def rk4(y0, x0, f, h, n, *args, **kwargs):
     yn[0, :] = y0
     xn[0] = x0
 
-    for n in np.arange(1, n + 1, 1):
-        yn[n, :], xn[n] = rk4_step(yn[n - 1, :], xn[n - 1], f, h, *args, **kwargs)
+    for m in np.arange(1, n + 1, 1):
+        yn[m, :], xn[m] = rk4_step(yn[m - 1, :], xn[m - 1], f, h, *args, **kwargs)
 
-    return (yn, xn)
+    return yn, xn
 
 
 # Be advised that the integration can take a while for large values of n (e.g >=10^5).
@@ -54,22 +54,32 @@ def deviation(y, _, r):
     return -r * y
 
 
+def exact(x,t):
+    return np.exp(-t*x)
+
+
 y_0 = np.array([1])
 x_0 = 0
-h = 0.01
+h = 0.001
 n = 1000
 r = 1
-arg = np.array([y_0, x_0, r])
+x = np.linspace(x_0, n * h + x_0, n)
 
 
 def main(y0, x0, f, h, n, *args, **kwargs):
-    yn, xn = rk4(y0, x0, f, h, n, *args, **kwargs)
+    hs=[0.001,0.01,0.1,1]
+    ns=[10000,1000,100,10]
     plt.figure(figsize=(8, 8))
-    plt.plot(xn, yn)
-    x = np.linspace(x_0, n * h + x_0, n)
-    plt.plot(x, np.exp(-x), label="analytical")
-    # plt.xscale("log")
-    plt.yscale("log")
+    err=np.zeros(len(hs))
+    for i in range(len(hs)):
+        h=hs[i]
+        n=ns[i]
+        yn, xn = rk4(y0, x0, f, h, n, *args, **kwargs)
+        err[i] = np.mean(np.abs(yn - exact(xn, r)) / exact(xn, r))
+        plt.plot(xn, yn, label='stepsize={}, mean error={}'.format(h,err[i]))
+    plt.plot(x, exact(x,r), label="analytical")
+    plt.legend()
+    plt.title('rk4 with different step sizes')
     plt.show()
 
 
