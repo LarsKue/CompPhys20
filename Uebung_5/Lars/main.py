89eface26b5a873442e16050b44135d62782ce2c
Lars Kue <lars@kuehmichel.de>
2020-05-22 14:09:09 +0200

    Praesenz 5

---
+++ Uebung_5/Lars/main.py
@@ -0,0 +1,33 @@
+
+import sys
+import numpy as np
+
+
+def praesenz() -> None:
+    epsilon = 1e-6
+
+    M = np.array([
+        [epsilon, 0.5],
+        [0.5, 0.5]
+    ])
+
+    y = (epsilon - 1) / (2 * epsilon + 1)
+    x = 0.5 - (epsilon - 1) / (2 * epsilon + 1)
+
+    v = np.array([x, y])
+
+    print(f"x = {x:.3f}")
+    print(f"y = {y:.3f}")
+
+    b = M @ v
+
+    print(f"M * v = {b}")
+
+
+def main(argv: list) -> int:
+    praesenz()
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv)
