453b8f049774820d4d7a436780105f0d2cad3719
Lars Kue <lars@kuehmichel.de>
2020-05-29 15:04:09 +0200

    Buggy Eigenvalues

--- Uebung_6/Lars/main.py
+++ Uebung_6/Lars/main.py
@@ -2,6 +2,7 @@
 import numpy as np
 from xalglib import smatrixtd
 from scipy.linalg import eigh_tridiagonal
+import math
 
 
 def is_symmetric(m, rtol=1e-5, atol=1e-8):
@@ -9,7 +10,7 @@ def is_symmetric(m, rtol=1e-5, atol=1e-8):
 
 
 def symmetric_to_tridiagonal(m):
-    return smatrixtd(list(m), len(m), False)
+    return smatrixtd(m.tolist(), len(m), False)
 
 
 def solve_eigenproblem(m):
@@ -21,52 +22,50 @@ def delta(n, m):
     return n == m
 
 
-def praesenz() -> None:
-    # M = [
-    #     [1, 2, 3, 4, 5],
-    #     [2, 1, 2, 3, 4],
-    #     [3, 2, 1, 2, 3],
-    #     [4, 3, 2, 1, 2],
-    #     [5, 4, 3, 2, 1]
-    # ]
-
-    M = [
-        [1, 3, 1, 1],
-        [2, 1, 3, 1],
-        [1, 2, 1, 3],
-        [1, 1, 2, 1]
-    ]
-
-    # tqli == scipy.linalg.eigh_tridiagonal
-    ls, vs = solve_eigenproblem(M)
-
-    np.set_printoptions(precision=2)
-
-    for l, v in zip(ls, vs):
-        print(f"lambda = {l:5.2f} for eigenvector v = {v}")
-
-
-def homework():
-    n = 0
-    N = 15
+def hamiltonian(n, l=0.1, N=30):
     Q = np.array([[np.sqrt(i + 1) * delta(i, j - 1) + np.sqrt(i) * delta(i, j + 1) for j in range(N)] for i in range(N)]) / np.sqrt(2)
-
-    print(Q)
-
     # Q^4
-    Q = Q @ Q @ Q @ Q
-
-    print(Q)
-
-    l = 0.1
+    Qt4 = Q @ Q @ Q @ Q
 
     h0 = (n + 0.5) * np.identity(N)
 
+    h = h0 + l * Qt4
 
+    return h
+
+
+def test_eigenvalue(a, l):
+    return math.isclose(np.linalg.det(a - l * np.identity(a.shape[0])), 0)
+
+
+def homework():
+    N = 30
+    l = 0.1
+
+    np.set_printoptions(precision=2)
+
+    for n in range(1):
+        h = hamiltonian(n, l, N)
+        # print("Hamiltonian:\n", h)
+        # print()
+
+        ls, vs = solve_eigenproblem(h)
+
+        for l, v in zip(ls, vs):
+            # print(f"Eigenvalue l = {l:5.2f} for Eigenvector v = {v}")
+
+            print(np.linalg.det(h - l * np.identity(N)))
+
+            # assert test_eigenvalue(h, l)
+
+
+        print()
+        # only diagonal terms
+        # h *= np.identity(N)
+        # print(h)
 
 
 def main(argv: list) -> int:
-    # praesenz()
     homework()
     return 0
 
