5a5b6cf63a570e3fdbf75f46a56d229ab7df9048
danikreuz <dani.winzeln@gmail.com>
2020-06-19 13:50:40 +0200

    new directory

---
+++ Uebung_9/Daniel/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import math
+
+
+def pra
+
+def main(argv: list) -> int:
+    homework()
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
