7da303a0d05b750510613ca1ba05ab49051c6ce5
Lars Kue <lars@kuehmichel.de>
2020-07-02 09:20:08 +0200

    Uebung_11 Setup

---
+++ Uebung_11/Lars/main.py
@@ -0,0 +1,19 @@
+
+
+def attendance() -> None:
+    pass
+
+
+def homework() -> None:
+    pass
+
+
+def main(argv: list) -> int:
+    attendance()
+    homework()
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
